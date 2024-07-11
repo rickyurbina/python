7e9050d5cc8148df6642e3acb03746d89e501e27
Ricardo Urbina Najera <rickyurbina@gmail.com>
2024-07-10 18:08:38 -0600

    Actualizacion

---
+++ turtle/main.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+tim = Turtle()
+tim.shape("turtle")
+
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
+
+
+
+
+pantalla = Screen()
+pantalla.exitonclick()
